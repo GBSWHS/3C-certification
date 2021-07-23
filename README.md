be5739e301e9ab8769b8f1077d7fc09fca3419c6
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2021-07-23 19:40:44 +0900

    feat: add navigation

---
+++ README.md
@@ -0,0 +1,2 @@
+## 3C-Certification
+3C 인증제 관리 사이트입니다.
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Topbar from '../components/Topbar'
+import Sidebar from '../components/Sidebar'
+
+export default function Home () {
+  return (
+    <div className="w-screen">
+      <Topbar />
+      <div className="inline-block w-full align-top p-10 text-4xl font-bold">
+        <h1 className="text-center">공지사항</h1>
+        
+      </div>
+    </div>
+  )
+}
